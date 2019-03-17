d0449d35d2f6a09d18008613bc4f365d957b849d
Alessandro Muroni <muronialessandro@gmail.com>
2019-03-17 16:50:04 +0100

    Working with inheritance/composition between classes

---
+++ v1/Exercise.py
@@ -0,0 +1,10 @@
+"""Exercise class, with exercise name and weight, reps and sets"""
+
+
+class Exercise:
+
+    def __init__(self, exercise_name, exercise_weight):
+        self.name = exercise_name
+        self.reps = 1
+        self.sets = 1
+        self.weight = exercise_weight
