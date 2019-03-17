a5fc0171c3e6e7353584eaadc956b0b63867a19d
Alessandro Muroni <muronialessandro@gmail.com>
2019-03-17 16:48:00 +0100

    Working with inheritance/composition between classes

--- v1/Main.py
+++ v1/Main.py
@@ -1,6 +1,10 @@
-"""main app file - v1 will focus on assiging attributes and gathering + showing data to the user"""
+"""main app file - v1 will focus on assigning attributes and gathering + showing data to the user"""
 
-from v1.classes import User
+from v1.User import *
+from v1.Exercise import *
 
-user1 = User(input("What's your name"), input("What's your age"), input("WHat's your weight?"))
+user1 = User(input("What's your name"), input("What's your age"), input("What's your weight?"))
 user1.presentation()
+
+exercise1 = Exercise(input("What exercise did you just do?"), input("With how much weight?"))
+user1.add_lift(exercise1)
---
+++ v1/User.py
@@ -0,0 +1,18 @@
+"""this will contain the main classes of the app"""
+from v1.Exercise import Exercise
+
+
+class User:
+
+    def __init__(self, name, age, weight):  # main user attrs
+        self.name = name
+        self. age = age
+        self.weight = weight
+        self.exercise = Exercise
+
+    def presentation(self):
+        print("User %s is %s years old and weighs %s kg" % (self.name, self.age, self.weight))
+
+    def add_lift(self, exercise):
+        self.user_lifts = [exercise]
+        print(self.name + " just did a " + exercise.name + " at " + exercise.weight)
