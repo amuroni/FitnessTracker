7a38a66d6bd2c634603b6d6afcd7580fb299f73d
Alessandro Muroni <muronialessandro@gmail.com>
2019-03-18 15:05:47 +0100

    Little progress in automating the user creation, still needs some work

--- v1/Main.py
+++ v1/Main.py
@@ -1,14 +1,17 @@
 """main app file - v1 will focus on assigning attributes and gathering + showing data to the user"""
 
 from v1.User import *
-from v1.Exercise import *
+# from v1.Exercise import *
 
-user1 = User(input("What's your name"), input("What's your age"), input("What's your weight?"))
-user1.presentation()
+# user1 = User(input("What's your name"), input("What's your age"), input("What's your weight?"))
+# user_list.presentation()
+# exercise1 = Exercise(input("What exercise did you do first?"), input("With how much weight?"))
+# exercise2 = Exercise(input("What exercise did you do for second?"), input("With how much weight?"))
+# exercise3 = Exercise(input("What exercise did you do the third time?"), input("With how much weight?"))
+# user1.add_lift(exercise1)
+# user1.add_lift(exercise2)
+# user1.add_lift(exercise3)
 
-exercise1 = Exercise(input("What exercise did you do first?"), input("With how much weight?"))
-exercise2 = Exercise(input("What exercise did you do for second?"), input("With how much weight?"))
-exercise3 = Exercise(input("What exercise did you do the third time?"), input("With how much weight?"))
-user1.add_lift(exercise1)
-user1.add_lift(exercise2)
-user1.add_lift(exercise3)
+create_user(int(input("How many users would you like to create?")))  # does not create a user obj?
+user1 = User("a", "1", "3")  # actually creates a user obj
+# note for future self -> check differences and build the def accordingly
--- v1/User.py
+++ v1/User.py
@@ -2,19 +2,31 @@
 from v1.Exercise import Exercise
 
 
+def create_user(account_number):
+    # user_list = []
+    # account_number = int(input("How many accounts would you like to create?"))
+    for i in range(0, account_number):
+        user_name = input("What's your name")
+        user_age = input("What's your age")
+        user_weight = input("What's your weight")
+        User(user_name, user_age, user_weight)
+
+
 class User:
 
     def __init__(self, name, age, weight):  # main user attrs
         self.name = name
-        self. age = age
+        self.age = age
         self.weight = weight
         self.exercise = Exercise  # composition instead of inheritance, more logical in this case
         self.user_exercises = []
+        self.user_list = []
 
     def presentation(self):
         print("User %s is %s years old and weighs %s kg" % (self.name, self.age, self.weight))
 
     def add_lift(self, exercise):
-        self.user_exercises.append(exercise)
-        print(self.name + " just did a " + exercise.name + " at " + exercise.weight)
-
+        exercise_number = int(input("How many exercises would you like to add?"))
+        for i in range(0, exercise_number):
+            self.user_exercises.append(Exercise(input("What exercise did you do ?"), input("With how much weight?")))
+        print(self.name + " tracked a " + exercise.name + " at " + exercise.weight)
