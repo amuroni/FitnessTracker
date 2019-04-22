05534189bfc9b870e0c53e01873c037c9581df87
Alessandro Muroni <muronialessandro@gmail.com>
2019-04-22 10:51:54 +0200

    Fixed user registration with uuid and multiple users/exercises

--- v4/Main.py
+++ v4/Main.py
@@ -1,7 +1,5 @@
-from v4.User import *
 import sqlite3
 import uuid
-
 
 # 1.create databases for users and exercises
 
@@ -10,32 +8,29 @@ cursor = db.cursor()
 db.execute('CREATE TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY, name TEXT, age INTEGER, weight INTEGER)')
 db.execute('CREATE TABLE IF NOT EXISTS exercises (id TEXT PRIMARY KEY, name TEXT, weight INTEGER)')
 
-# 2. ask user input for n. of users to create
+# 2. ask user input for n. of users to create + n. of exercises to add
 users_num = int(input("How many users would you like to create?"))
-user_list = []
 for user in range(0, users_num):
-    user_list.append(User(input("What's your name"), input("What's your age"), input("What's your weight")))
-#  note: need to find a way to generate user, exercise, add to db, connect UUID and proceed to next user
-# need to modify the for loop to create ONE USER with its exercises, then proceed to the next one (if any)
-# 3. for each user created, add to db then ask user input for exercises performed and add to db
-for user in user_list:
-    db.execute("INSERT INTO users (id, name, age, weight) VALUES (?, ?, ?, ?)",  # add each user to db
-               (str(uuid.uuid1()), user.name, user.age, user.weight))
+    user_name = input("What's your name")
+    user_age = input("What's your age")
+    user_weight = input("What's your weight")
+    user_id = str(uuid.uuid1())
+    db.execute("INSERT INTO users (id, name, age, weight) VALUES (?, ?, ?, ?)",  # add each user attr to db
+               (user_id, user_name, user_age, user_weight))
     db.commit()  # commit the user insert into the users table
-    user.add_lift(int(input("Hello %s, how many exercises do you want to track?" % user.name)))
-    for exercise in user.exercise:
-        cursor.execute("SELECT id FROM users")
-        user_id = str(cursor.fetchone()[0])
+    exercise_num = (int(input("Hello %s, how many exercises do you want to track?" % user_name)))
+    # 2.1 add exercise for each user with the user_id to assign a user to each exercise
+    for exercise in range(0, exercise_num):
+        exercise_name = input("What exercise did you do?")
+        exercise_weight = input("With hou much weight?")
+        exercise_id = user_id
         db.execute("INSERT INTO exercises (id, name, weight) VALUES (?, ?, ?)",   # add each exercise to db
-                   (user_id, exercise.exercise_name, exercise.exercise_weight))
+                   (exercise_id, exercise_name, exercise_weight))
         db.commit()  # commit the exercise insert into the exercises table
-
-# 4. print registered username from db to validate registration process
-cursor.execute("SELECT * FROM users")
-for row in cursor:
-    print("Registered user " + user.name)  # weird error check, still works as expected?
+    # 2.2 confirm user registration
+    print("Registered user " + user_name)
     print("-" * 20)
 
-# 5. close cursor and db
+# 3. close cursor and db
 cursor.close()
 db.close()
