63b9395e28c6b9f28396c3a2d2e0dc2d1eb475c7
Alessandro Muroni <muronialessandro@gmail.com>
2019-04-25 11:08:03 +0200

    Modified database.py file, defined specific functions for user creation and update. Much cleaner code.

--- v4/Database.py
+++ v4/Database.py
@@ -10,7 +10,7 @@ def create_database():
     db.execute('CREATE TABLE IF NOT EXISTS exercises (user_id TEXT, name TEXT, weight INTEGER)')
 
 
-def add_user(users_num):
+def create_user(users_num):
     for user in range(0, users_num):
         user_name = input("What's your name")
         cursor.execute("SELECT * FROM users WHERE name = ?", (user_name,))
@@ -21,25 +21,34 @@ def add_user(users_num):
             db.execute("INSERT INTO users (id, name, age, weight) VALUES (?, ?, ?, ?)",  # add each user attr to db
                        (user_id, user_name, user_age, user_weight))
             db.commit()
-            exercise_num = (int(input("Hello %s, how many exercises do you want to track?" % user_name)))
-            for exercise in range(0, exercise_num):
-                exercise_name = input("What exercise did you do?")
-                exercise_weight = input("With how much weight (kg)?")
-                exercise_id = user_id
-                db.execute("INSERT INTO exercises (user_id, name, weight) VALUES (?, ?, ?)",   # add each exercise to db
-                           (exercise_id, exercise_name, exercise_weight))
-                db.commit()
-            print("Registered user " + user_name)
+            add_exercise(user_id, user_name)
+            print("-" * 20)
+            print("Successful registration of user " + user_name)
             print("-" * 20)
         else:
-            print("User %s already exists" % user_name)
-            cursor.execute("SELECT id FROM users WHERE name = ?", (user_name,))
-            user_id = cursor.fetchone()[0]
-            exercise_num = (int(input("Hello %s, how many exercises do you want to track?" % user_name)))
-            for exercise in range(0, exercise_num):
-                exercise_name = input("What exercise did you do?")
-                exercise_weight = input("With how much weight (kg)?")
-                exercise_id = user_id
-                db.execute("INSERT INTO exercises (user_id, name, weight) VALUES (?, ?, ?)",
-                           (exercise_id, exercise_name, exercise_weight))
-                db.commit()
+            update_user(user_name)
+            print("-" * 20)
+            print("Update successful for user " + user_name)
+            print("-" * 20)
+
+
+def update_user(user_name):
+    print("User %s already exists" % user_name)
+    cursor.execute("SELECT id FROM users WHERE name = ?", (user_name,))
+    user_id = cursor.fetchone()[0]
+    add_exercise(user_id, user_name)
+
+
+def add_exercise(user_id, user_name):
+    exercise_num = (int(input("Hello %s, how many exercises do you want to track?" % user_name)))
+    for exercise in range(0, exercise_num):
+        exercise_name = input("What exercise did you do?")
+        exercise_weight = input("With how much weight (kg)?")
+        exercise_id = user_id
+        db.execute("INSERT INTO exercises (user_id, name, weight) VALUES (?, ?, ?)",
+                   (exercise_id, exercise_name, exercise_weight))
+        db.commit()
+        print("-" * 20)
+        print("Successfully registered exercise '%s' with %skg weight to user %s"
+              % (exercise_name, exercise_weight, user_name))
+        print("-" * 20)
--- v4/Main.py
+++ v4/Main.py
@@ -3,8 +3,8 @@ from v4.Database import *
 # 1.create databases for users and exercises
 create_database()
 # 2. ask user input for n. of users to create
-users_num = int(input("How many users would you like to create?"))
-add_user(users_num)
+users_num = int(input("How many users would you like to create/update?"))
+create_user(users_num)
 # 3. close cursor and db
 cursor.close()
 db.close()
